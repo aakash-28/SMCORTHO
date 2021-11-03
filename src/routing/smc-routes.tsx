09b0f9824eaf08263e4dd9e318a9bdd1a6dd27d8
CodeSeeker99 <rishabh29b@gmail.com>
2021-11-03 22:53:36 +0530

    Base2

--- src/routing/smc-routes.tsx
+++ src/routing/smc-routes.tsx
@@ -1,7 +1,12 @@
 import { IRoute } from "../interfaces/routing";
+import HomePage from "../pages/home/Home";
 import PatientsPage from "../pages/patients/Patients";
 
 export const smcRoutes: IRoute[] = [
+    {
+      path: 'default',
+      component: HomePage
+    },
     {
       path: 'patients',
       component: PatientsPage
